3143eb327e4480cbfa8d3d65a0b2092ca9d7f4a4
Upthrust22 <105308552+Upthrust22@users.noreply.github.com>
2023-02-07 18:27:18 -0800

    First Commit
    First Commit of Password generator with password input length

---
+++ app.js
@@ -0,0 +1,58 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+// Selectors
+const generatPWBtn = document.querySelector('#generate-pw-btn');
+const displayPW1 = document.querySelector('#display-pw1');
+const displayPW2 = document.querySelector('#display-pw2');
+const pwLength = document.querySelector('#pw-length');
+
+
+// Event listener for Generate password
+generatPWBtn.addEventListener('click', function() {
+            displayPW1.textContent = generatePasswordOne();
+
+            displayPW2.textContent = generatePasswordTwo();
+});
+
+// Generating Random Character
+function getRandomChar() {
+    let randomLetter = characters[Math.floor(Math.random() * characters.length)];
+    return randomLetter;
+}
+
+// Generate password one
+function generatePasswordOne() {
+    let passwordOne = '';
+
+    if (pwLength.value === '') {
+        for (let i = 0; i < 4; i++){
+            passwordOne += getRandomChar();
+        }
+    } else {
+        for (let i = 0; i < pwLength.value; i++){
+            passwordOne += getRandomChar();
+        }
+    }
+    return passwordOne;
+}
+
+// Generate password 2
+function generatePasswordTwo() {
+    let passwordTwo = '';
+
+    if (pwLength.value === '') {
+        for (let i = 0; i < 4; i++){
+            passwordTwo += getRandomChar();
+        }
+    } else {
+        for (let i = 0; i < pwLength.value; i++){
+            passwordTwo += getRandomChar();
+        }
+    }
+    return passwordTwo;
+}
+
+
+
+
+
